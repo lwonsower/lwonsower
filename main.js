431823538106e29d21932fc83e1dbfd41e9e5c63
lwonsower <lwonsower@gmail.com>
2016-08-20 19:13:49 -0700

    fixed angular changes

--- main.js
+++ main.js
@@ -1,36 +1,30 @@
-angular.module('main', ['pages', 'ngroute' ])
+var main = angular.module('main', ['ngRoute'])
 
 .config(function($routeProvider){
 	$routeProvider
 		.when('/blog', {
-			templateUrl: 'pages/blog.html',
+			templateUrl: './pages/blog.html',
 			controller: 'BlogController'
 		})
 		.when('/contact', {
-			templateUrl: 'pages/contact.html',
+			templateUrl: './pages/contact.html',
 			controller: 'ContactController'
 		})
 		.when('/cv', {
-			templateUrl: 'pages/cv.html',
+			templateUrl: './pages/cv.html',
 			controller: 'CVController'
 		})
 		.otherwise({redirectTo: '/index.html' });
-});
-
-angular.module('pages', []{
-	.controller('CVController', function($scope){
-		$scope.message="Hello!!!"
-	})
 })
 
-angular.module('pages', []{
-	.controller('BlogController', function($scope){
+.controller('CVController', function($scope){
 		$scope.message="Hello!!!"
-	})
 })
 
-angular.module('pages', []{
-	.controller('ContactController', function($scope){
-		$scope.message="Hello!!!"
-	})
+.controller('BlogController', function($scope){
+	$scope.message="Hello!!!"
 })
+
+.controller('ContactController', function($scope){
+	$scope.message="Hello!!!"
+})()
