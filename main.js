9d6cb014e3dc170db598cf43e017b77871d3dc78
lwonsower <lwonsower@gmail.com>
2016-09-11 13:52:43 -0700

    [Feature] routing

--- main.js
+++ main.js
@@ -2,29 +2,64 @@ var main = angular.module('main', ['ngRoute'])
 
 .config(function($routeProvider){
 	$routeProvider
-		.when('/blog', {
-			templateUrl: './pages/blog.html',
-			controller: 'BlogController'
-		})
 		.when('/contact', {
-			templateUrl: './pages/contact.html',
+			templateUrl: '/pages/contact.html',
 			controller: 'ContactController'
 		})
 		.when('/cv', {
-			templateUrl: './pages/cv.html',
+			templateUrl: '/pages/cv.html',
 			controller: 'CVController'
+		})
+		.when('/socialmedia', {
+			templateUrl: '/pages/socialmedia.html',
+			controller: 'SocialMediaController'
 		})
 		.otherwise({redirectTo: '/index.html' });
 })
 
 .controller('CVController', function($scope){
-		$scope.message="Hello!!!"
-})
-
-.controller('BlogController', function($scope){
 	$scope.message="Hello!!!"
 })
 
 .controller('ContactController', function($scope){
 	$scope.message="Hello!!!"
+})
+
+.controller('SocialMediaController', function($scope, GetFeed){
+	$scope.content = [];
+	$scope.getData = () => {
+		GetFeed.tumblrFeed().then(function(data){
+			$scope.content.push(data);
+			GetFeed.instagramFeed().then(function(data2){
+				$scope.content.push(data2)
+					console.log("inside controller", $scope.content)
+			})
+		});
+	}	
+})
+
+.factory(`GetFeed`, ($http, $window) => {
+	let tumblrFeed = () => {
+    return $http({
+      method: 'GET',
+      url: '/tumblr'
+    }).then( res => {
+    	console.log("tumblr", res)
+      return res;
+    })
+  }
+  let instagramFeed = () => {
+    return $http({
+      method: 'GET',
+      url: '/instagram'
+    }).then(res => {
+    	console.log("insta", res)
+      return res;
+    })
+  }
+
+  return {
+  	tumblrFeed: tumblrFeed,
+  	instagramFeed: instagramFeed
+  }
 });
---
+++ server/apikeys.js
@@ -0,0 +1,13 @@
+"use strict";
+
+module.exports = {
+	tumblr: {
+	  consumer_key: 'luEr2hNSAnqAleDZ5ny3clJ8oIDC6SvMLCMfw8GXHUvRbJXeD0',
+	  consumer_secret: '252l7z5WICul7yKe6nG12b0vMSlHFO4tF5gNOfltViv04Agw7b',
+	  token: 'CM3t5HS7TmKGqq56ePOHBCIyXyoFrmx7Lokcts8qwrOxKziiUJ',
+	  token_secret: '2GtZlnfXPIGODHFef82dssaxQ4WPHxJQlB0pTKquyI8tsJA80N'
+	},
+	instagram: {
+		api_key: '7a9166fe662d4ced98bfbb45ccf81f45'
+	}
+}
